a60b292e72db24045bde0ba73b1a202606fe0b64
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-07-30 17:14:52 +0530

    Update 3.cpp

--- week2/3.cpp
+++ week2/3.cpp
@@ -1,1 +1,28 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t,i;
+	cin>>t;
+	while(t--)
+	{
+		int n,i,k,j,c=0;
+		cin>>n;
+		int a[n];
+		for(i=1;i<=n;i++)
+		cin>>a[i];
+		cout<<endl<<"KEY ELEMENT = ";
+		cin>>k;
+		for(i=1;i<n;i++)
+		{
+			for(j=i+1;j<=n;j++)
+			{
+				if(abs(a[i]-a[j])==k)
+				{
+					c++;
+				}
+			}
+		}
+		cout<<"Count = "<<c<<endl;
+	}
+}

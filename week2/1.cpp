68c5d3e909edbe7821a392fbd34161d8a48fcbf4
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-07-30 18:27:14 +0530

    Update 1.cpp

--- week2/1.cpp
+++ week2/1.cpp
@@ -1,1 +1,32 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,i,c=0,k;
+		cin>>n;
+		int a[n];
+		for(i=0;i<n;i++)
+		cin>>a[i];
+		cout<<endl<<"KEY ELEMENT = ";
+		cin>>k;
+		for(i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+			c++;	
+			}
+			if(a[i]>k)
+			{
+				break;
+			}
+		}
+		if(c==0)
+		cout<<"Key not present"<<endl;
+		else
+		cout<<k<<"-"<<c<<endl;
+	}
+}

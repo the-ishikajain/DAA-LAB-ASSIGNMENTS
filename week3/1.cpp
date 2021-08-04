1ae75c3d557d1e167efe815c9902f8274ebcdbd7
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-08-04 19:29:19 +0530

    Update 1.cpp

--- week3/1.cpp
+++ week3/1.cpp
@@ -1,1 +1,34 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t,i,n,j,t1,comp,shifts;
+	cin>>t;
+	while(t--)
+	{
+		comp=0;
+		shifts=0;
+		cin>>n;
+		int a[n];
+		for(i=0;i<n;i++)
+		cin>>a[i];
+		for(i=1;i<n;i++)
+		{
+			t1=a[i];
+			j=i-1;
+			while(j>=0 && a[j]>t1)
+			{
+				a[j+1]=a[j];
+				j--;
+				comp++;
+			}
+			a[j+1]=t1;
+			shifts++;
+		}
+		cout<<endl<<"OUTPUT :"<<endl;
+		for(i=0;i<n;i++)
+		cout<<a[i]<<" ";
+		cout<<endl<<"COMPARISONS ="<<comp<<endl;
+		cout<<"SHIFTS ="<<shifts+comp<<endl;
+	}
+}

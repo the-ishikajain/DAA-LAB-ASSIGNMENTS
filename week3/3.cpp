646b330eb7639603a930cc055ea7d30b286efe7e
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-08-04 21:43:07 +0530

    Update 3.cpp

--- week3/3.cpp
+++ week3/3.cpp
@@ -1,1 +1,29 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t,i,j,n,flag;
+	cin>>t;
+	while(t--)
+	{
+		flag=0;
+		cin>>n;
+		int a[n];
+		for(i=0;i<n;i++)
+		cin>>a[i];
+		sort(a,a+n);
+		for(i=0;i<n;i++)
+		{
+			if(a[i]==a[i+1])
+			{
+			flag=1;
+			break;
+			}
+		}
+		if(flag==0)
+		cout<<"NO"<<endl;
+		else
+		cout<<"YES"<<endl;
+		
+	}
+}

c0312cc6cca31cc5f46f9a9e4dd0054487c0ef67
Ishika Jain <75484706+the-ishikajain@users.noreply.github.com>
2021-08-04 20:10:41 +0530

    Update 2.cpp

--- week3/2.cpp
+++ week3/2.cpp
@@ -1,1 +1,36 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int min,i,j,n,t,swaps,comp;
+	cin>>t;
+	while(t--)
+	{
+		swaps=0;
+		comp=0;
+		cin>>n;
+		int a[n];
+		for(i=0;i<n;i++)
+		cin>>a[i];
+		for(i=0;i<n-1;i++)
+		{
+			min=i;
+			for(j=i+1;j<n;j++)
+			{
+				if(a[j]<a[min])
+				{
+					min=j;
+			    }
+			    comp++;
+			}
+			int t=a[i];
+			a[i]=a[min];
+			a[min]=t;
+			swaps++;
+		}
+		for(i=0;i<n;i++)
+		cout<<a[i]<<" ";
+		cout<<endl<<"COMPARISONS ="<<comp<<endl;
+		cout<<"SWAPS ="<<swaps<<endl;
+	}
+}
